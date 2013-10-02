d03ac78dedd7bbc32d8043d632647825d413f392
Teo Dell'Amico <teo.dellamico@gmail.com>
2013-10-02 15:07:17 -0700

    added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rbghub::Application.routes.draw do
+
+get 'home' => 'pages#home'
+get 'about' => 'pages#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+ root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
