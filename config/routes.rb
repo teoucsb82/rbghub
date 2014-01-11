0b55a9a1b160795e868a644a75401dfd2bd45158
Teo Dell'Amico <teo.dellamico@gmail.com>
2014-01-10 20:54:48 -0800

    redesigned homepage

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rbghub::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
- root 'pages#home'
+ root 'teamstats#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
